e10935472d7a7c505f4d23649d993e1dee164ca7
zlicdt <zhuhaoming135@outlook.com>
2026-01-03 10:29:12 +0800

    feat: Add tap file

---
+++ Formula/apple-smi.rb
@@ -0,0 +1,21 @@
+class AppleSmi < Formula
+  desc "nvidia-smi like GPU inspector for Apple Silicon on macOS"
+  homepage "https://github.com/zlicdt/apple-smi"
+  license "MIT"
+
+  on_intel do
+    odie "apple-smi only supports Apple Silicon Macs"
+  end
+
+  version "1.3.3"
+  url "https://github.com/zlicdt/apple-smi/releases/download/v#{version}/apple-smi", using: :nounzip
+  sha256 "..."
+
+  def install
+    bin.install "apple-smi"
+  end
+
+  test do
+    system "#{bin}/apple-smi", "--help"
+  end
+end
